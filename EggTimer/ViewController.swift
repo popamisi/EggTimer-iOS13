9f8f37ee0f8423437e989685883d47a8f5e07a2b
popamisi <96704995+popamisi@users.noreply.github.com>
2022-02-09 21:07:53 +0100

    first test

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var timer: Timer?
+    var counter = 3
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+        //print(eggTimes[hardness]!)
+        
+        func seconds() {
+            counter = eggTimes[hardness]!
+            //print(eggTimes[hardness]!)
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        seconds()
+    }
+    
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+            
+        }
+        
+        if counter == 0 {
+            print("0 seconds")
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    
 }
